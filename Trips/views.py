e6289ac17117087b4cff9ecb7fd68efaa28d8814
LuisBell0 <lalfon11@gmail.com>
2024-04-24 21:18:32 -0400

    Commit 4: Added Notes list and details.

--- Trips/views.py
+++ Trips/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 from .models import Trip, Note
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def home_page(request):
+    return render(request, 'trips/home_page.html')
 
+@login_required
 def trip_list(request):
     trips = Trip.objects.filter(owner=request.user)
     context = {
@@ -18,9 +22,34 @@ class TripCreateView(CreateView):
     model = Trip
     success_url = reverse_lazy('trip-list')
     fields = ['city', 'country', 'start_date', 'end_date']
-    #template named model_form.html
+    # template named model_form.html
 
     def form_valid(self, form):
-        #owner field = logged in user
+        # owner field = logged in user
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
+
+class TripDetailView(DetailView):
+    model = Trip
+    # template: model_detail.html
+    # data stored on Trip - also have the notes data
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        trip = context['object']
+        notes = trip.notes.all()
+        context['notes'] = notes
+        return context
+
+
+class NoteDetailView(DetailView):
+    model = Note
+
+
+class NoteListView(ListView):
+    model = Note
+    # TEMPLATE: model_list.html
+    def get_queryset(self):
+        queryset = Note.objects.filter(trip__owner=self.request.user)
+        return queryset
